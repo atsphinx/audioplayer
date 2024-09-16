b7ec4793261a83a985d2122558e71efdb5641c03
Kazuya Takei <myself@attakei.net>
2024-09-16 19:37:09 +0900

    doc: Support japanese

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,7 @@
+import os
+
 from atsphinx.audioplayer import __version__ as version
+from atsphinx.mini18n import get_template_dir as get_mini18n_template_dir
 
 # -- Project information
 project = "atsphinx-audioplayer"
@@ -9,23 +12,46 @@ release = version
 # -- General configuration
 extensions = [
     "atsphinx.audioplayer",
+    "atsphinx.mini18n",
     "sphinx.ext.intersphinx",
     "sphinx.ext.todo",
     "sphinx_copybutton",
     "sphinx_design",
 ]
-templates_path = ["_templates"]
+templates_path = ["_templates", get_mini18n_template_dir()]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for i18n
+locale_dirs = ["_locales"]
+gettext_compact = False
+gettext_language_team = "Kazuya Takei <myself@attakei.net>"
+gettext_last_translator = os.environ.get("SPHINXINTL_TRANSLATOR", None)
 
 # -- Options for HTML output
 html_theme = "furo"
 html_static_path = ["_static"]
+html_title = f"{project} v{release}"
+html_sidebars = {
+    "**": [
+        "sidebar/scroll-start.html",
+        "sidebar/brand.html",
+        "mini18n/snippets/select-lang.html",
+        "sidebar/search.html",
+        "sidebar/navigation.html",
+        "sidebar/ethical-ads.html",
+        "sidebar/scroll-end.html",
+    ]
+}
 
 # -- Extension configuration
 # For sphinx.ext.intersphinx
 intersphinx_mapping = {
     "sphinx": ("https://www.sphinx-doc.org/en/master", None),
 }
+# atsphinx.mini18n
+mini18n_default_language = "en"
+mini18n_support_languages = ["en", "ja"]
+mini18n_select_lang_label = ""
 
 
 def setup(app):
